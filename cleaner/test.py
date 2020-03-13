52d1a196689a77928090e5deb6bf0e2b97e18a68
yuliachornenko <chornenko1234@gmail.com>
2020-03-14 01:48:38 +0200

    Fifth commit.

--- cleaner/test.py
+++ cleaner/test.py
@@ -1,24 +1,23 @@
 import pandas as pd
 import re
 
-df = pd.read_csv('data/data.csv')
+df = pd.read_csv('../data/data.csv')
 
 df.drop(df[(df.category == '1') | (df.category == '0')].index, inplace=True)
 
-df = df[~df['text'].str.contains('?')]
-print(df)
 
-# i=0
-# for line in df.text:
-#     match = re.search('[?]', line)
-#     if (df.category[i] == 'Positive' or df.category[i] == 'Negative') and match != None :
-#         print(match, i)
-#     i+=1
+i = 0
+for line in df.text:
+    match = re.search('[?]', line)
+    try:
+        match = re.search('[?]', line)
+        if (df.category[i] == 'Positive'  or df.category[i] == 'Negative') and match != None :
+            df.drop(df.index[i])
+            print(line)
+    except:
+        None
 
-
-# f=866
-# print(df.text[f], df.category[f])
-
+    i += 1
 
 
 n = 65
@@ -27,7 +26,7 @@ positive = df[(df.category == 'Positive')].sample(frac=1).reset_index(drop=True)
 negative = df[(df.category == 'Negative')].sample(frac=1).reset_index(drop=True)[:n]
 hotline = df[(df.category == 'Hotline')].sample(frac=1).reset_index(drop=True)[:n]
 hooligan = df[(df.category == 'Hooligan')].sample(frac=1).reset_index(drop=True)[:n]
-siteandcoins = pd.read_csv('data/coins.csv')[:n]
+siteandcoins = pd.read_csv('../data/coins.csv')[:n]
 
 DF = pd.concat([positive, negative, hotline, hooligan, siteandcoins])
-DF.to_csv('data/category.csv', index=False)
+DF.to_csv('../data/category.csv', index=False)
