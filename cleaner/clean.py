80a26039460d93ae12c5da4d741f39f3250727e9
aleksandrlozko <44771725+aleksandrlozko@users.noreply.github.com>
2020-03-24 22:03:48 +0200

    Revert "23th commit."
    This reverts commit 13978d7dd65a281db2ba95dd927a74efb56b522c.
    
    # Conflicts:
    #	data/letters1.txt
    #	test_model(nbu test data with answ).py

--- cleaner/clean.py
+++ cleaner/clean.py
@@ -10,12 +10,10 @@ n = 100
 
 positive = tc.CleanText.detect_lang(df, 'Positive', n).sample(frac=1).reset_index(drop=True)
 negative = tc.CleanText.detect_lang(df, 'Negative', n).sample(frac=1).reset_index(drop=True)
-hotline = tc.CleanText.detect_lang(df, 'Hotline', n)
+hotline = tc.CleanText.detect_lang(df, 'Hotline', n).sample(frac=1).reset_index(drop=True)
 hooligan = df[(df.category == 'Hooligan')].sample(frac=1).reset_index(drop=True)[:n]
 offer = df[(df.category == 'Offer')].sample(frac=1).reset_index(drop=True)[:n]
-siteandcoins1 = tc.CleanText.detect_lang(df, 'SiteAndCoins', 62)
-siteandcoins2 = pd.read_csv('../data/coins.csv')
-siteandcoins = pd.concat([siteandcoins1, siteandcoins2])[:n]
+siteandcoins = pd.read_csv('../data/coins.csv')[:n]
 
 DF = pd.concat([positive, negative, hotline, hooligan, offer, siteandcoins])
 DF.to_csv('../data/category.csv', index=False)
